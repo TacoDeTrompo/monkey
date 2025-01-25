fcb39dc86192f6386722ce87ae1470fa056801b7
TacoDeTrompo <rodrguez.samuel@gmail.com>
2025-01-24 23:26:16 -0600

    added player state machine and scene

---
+++ Scripts/Player/PlayerStateMachine.gd
@@ -0,0 +1,13 @@
+extends "res://State_Machine.gd"
+
+@export var PlayerNode: CharacterBody2D
+@export var PhysicsData: PlayerPhysicsData
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
